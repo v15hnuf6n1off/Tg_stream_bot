d24a33b663fc34e0ff20bac47887974e84078650
VISHNU <Vishnuvishva2434@gmail.com>
2024-02-23 09:19:01 +0530

    Update files.py

--- bot/plugins/files.py
+++ bot/plugins/files.py
@@ -27,11 +27,11 @@ async def user_file_handler(event: NewMessage.Event | Message):
             message= MediaLinksText % {'dl_link': dl_link, 'tg_link': tg_link, 'tg_link': tg_link, 'stream_link': stream_link},
             buttons=[
                 [
-                    Button.url('**📥Download**', dl_link),
-                    Button.url('**📺Stream**', stream_link)
+                    Button.url('📥Download', dl_link),
+                    Button.url('📺Stream', stream_link)
                 ],
                 [
-                    Button.url('**📁Get File**',deep_link),
+                    Button.url('📁Get File',\ndeep_link),
                     Button.inline('🗑Revoke', f'rm_{message_id}_{secret_code}')
                 ]
             ]
@@ -41,8 +41,8 @@ async def user_file_handler(event: NewMessage.Event | Message):
             message=FileLinksText % {'dl_link': dl_link, 'tg_link': tg_link},
             buttons=[
                 [
-                    Button.url('**📥Download**', dl_link),
-                    Button.url('**📁Get File**', deep_link)
+                    Button.url('📥Download', dl_link),
+                    Button.url('📁Get File', deep_link)
                 ],
                 [
                     Button.inline('**🗑Revoke**', f'rm_{message_id}_{secret_code}')
@@ -70,8 +70,8 @@ async def channel_file_handler(event: NewMessage.Event | Message):
         try:
             await event.edit(
                 buttons=[
-                    [Button.url("**📥Download**", dl_link), Button.url("**📺Stream", stream_link)],
-                    [Button.url("**📁Get File**", tg_link)],
+                    [Button.url("📥Download", dl_link), Button.url("📺Stream", stream_link)],
+                    [Button.url("📁Get File", tg_link)],
                 ]
             )
         except (
@@ -84,7 +84,7 @@ async def channel_file_handler(event: NewMessage.Event | Message):
         try:
             await event.edit(
                 buttons=[
-                    [Button.url("**📥Download**", dl_link), Button.url("**📁Get File**", tg_link)]
+                    [Button.url("📥Download", dl_link), Button.url("📁Get File", tg_link)]
                 ]
             )
         except (
