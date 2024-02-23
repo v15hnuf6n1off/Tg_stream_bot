fff47db8905aa7e1e721db0264f2230f5af71527
VISHNU <Vishnuvishva2434@gmail.com>
2024-02-23 09:33:44 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -11,7 +11,7 @@ class Telegram:
     SECRET_CODE_LENGTH = int(env.get("SECRET_CODE_LENGTH", 12))
 
 class Server:
-    BASE_URL = env.get("BASE_URL", "http://106.197.113.221:8080")
+    BASE_URL = env.get("BASE_URL", "https://test091.a1phers.workers.dev")
     BIND_ADDRESS = env.get("BIND_ADDRESS", "0.0.0.0")
     PORT = int(env.get("PORT", 8080))
 
